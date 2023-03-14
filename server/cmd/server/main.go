9f664bef731ab3fa5e68b39f5a1e420eb6be0650
elena <edementieva@list.ru>
2023-03-14 14:44:05 +0300

    fix server

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,6 +1,11 @@
 package main
 
+import (
+	"chat/server/internal/server"
+	"net/http"
+)
+
 func main() {
-	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/", server.HelloServer)
 	http.ListenAndServe(":8080", nil)
 }
